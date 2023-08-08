aa3763f2a78ef878a864dcfa3102ad9c33fab26d
Zacharie Montreuil <zacharie.montreuil@mitt.ca>
2023-08-08 14:04:31 -0500

    Add project files.

---
+++ InterfaceDemo/Interface1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace InterfaceDemo
+{
+
+    public interface ISayHello
+    {
+        string SayHello();
+    }
+
+    public interface IMakeBreakfast
+    {
+        string MakeBreakfast();
+    }
+
+
+    public abstract class Human
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    _name = value;
+                }
+            }
+        }
+        public void CountToTen()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(10);
+            }
+        }
+    }
+
+
+    public class HumanFromCanada : Human, IMakeBreakfast, ISayHello
+    {
+        public string MakeBreakfast()
+        {
+            return "Pancakes, Maple Syrup, and Back Bacon";
+        }
+
+        public string SayHello()
+        {
+            return "Hey there bud!";
+        }
+    }
+
+    public class DuckFromCanada: ISayHello
+    {
+        public string SayHello()
+        {
+            return "QUACK";
+        }
+    }
+}
---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InterfaceDemo;
+
+List<ISayHello> helloSayers = new List<ISayHello>()
+{
+    new HumanFromCanada(),
+    new DuckFromCanada()
+};
+
+ISayHello helloSayer;
+
+IMakeBreakfast breakfastMaker;
+
+
+string type = "anything else";
+
+if(type == "person")
+{
+    helloSayer = new HumanFromCanada(); 
+} else
+{
+    helloSayer = new DuckFromCanada();
+}
+
+Console.WriteLine(helloSayer.SayHello());
